9b1be52d9176efb229ad9f4453fe4b4bec49e4e8
Naresh_Jindal <naresh_jindal@epam.com>
2023-05-19 11:45:22 +0530

    Refactored code

--- src/main/java/poc/tree/binary/mirror/MirrorBinaryTree.java
+++ src/main/java/poc/tree/binary/mirror/MirrorBinaryTree.java
@@ -70,4 +70,34 @@ public class MirrorBinaryTree {
             printTree(root.getRight());
         }
     }
+
+    private static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        public int getData() {
+            return data;
+        }
+
+        public void setData(int data) {
+            this.data = data;
+        }
+
+        public Node getLeft() {
+            return left;
+        }
+
+        public void setLeft(Node left) {
+            this.left = left;
+        }
+
+        public Node getRight() {
+            return right;
+        }
+
+        public void setRight(Node right) {
+            this.right = right;
+        }
+    }
 }
--- src/main/java/poc/tree/generic/mirror/MirrorGenericTree.java
+++ src/main/java/poc/tree/generic/mirror/MirrorGenericTree.java
@@ -3,6 +3,8 @@ package poc.tree.generic.mirror;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class MirrorGenericTree {
 
@@ -68,5 +70,42 @@ public class MirrorGenericTree {
             }
         }
 
+    }
+    private static class Node {
+        int data;
+        List<Node> children;
+
+        public int getData() {
+            return data;
+        }
+
+        public void setData(int data) {
+            this.data = data;
+        }
+
+        public List<Node> getChildren() {
+            return children;
+        }
+
+        public void setChildren(List<Node> children) {
+            this.children = children;
+        }
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "data=" + data +
+                    ", children=" + children +
+                    '}';
+        }
+
+        public void printChildren() {
+            if (this.getChildren() != null && this.getChildren().size() > 0) {
+                final String collect = this.getChildren().stream()
+                        .map(child -> String.valueOf(child.getData()))
+                        .collect(Collectors.joining(","));
+                System.out.print(collect);
+            }
+        }
     }
 }
